a1f767344454869bdbbf599efd9fa52ee761b22b
DmitriySavin <debilv556@gmail.com>
2026-04-25 10:11:27 +0300

    фвв

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,19 @@
-import { useSelector , useDispatch } from "react-redux"
+import { useSelector, useDispatch } from "react-redux";
 import { decrement, increment } from "../redux/store";
 
 export const Layout = () => {
-    const value = useSelector(state => state.myValue);
-const dispatch = useDispatch()
+  const value = useSelector((state) => state.myValue);
+  const dispatch = useDispatch();
 
-
-    return (
-        <div>
-            {value.value}
-            <button type="button" onClick={() => dispatch(increment(25))}>Add</button>
-            <button type="button" onClick={() => dispatch(decrement(25))}>Delete</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      {value.value}
+      <button type="button" onClick={() => dispatch(increment(25))}>
+        Add
+      </button>
+      <button type="button" onClick={() => dispatch(decrement(25))}>
+        Delete
+      </button>
+    </div>
+  );
+};
